329cf8fd4156bbee4c1910fce3783bef56fe51af
Dilip Kumar <kumar.dilip.it99@gmail.com>
2025-01-24 00:34:00 +0530

    initial commit 01

---
+++ src/test/java/com/practice/test/HelloTest.java
@@ -0,0 +1,22 @@
+package com.practice.test;
+
+import org.testng.annotations.Test;
+
+public class HelloTest {
+
+	public HelloTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	@Test
+	public void m1()
+	{
+	  System.out.println("HelloDilip");	
+	}
+
+}
